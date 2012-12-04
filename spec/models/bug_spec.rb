5050400d7fa32adf059ced584b01478e774a28cb
Ren Quinn <renquinn@gmail.com>
2012-12-03 17:21:01 -0700

    [RED] Check for my bugs scope

--- spec/models/bug_spec.rb
+++ spec/models/bug_spec.rb
@@ -5,5 +5,14 @@ describe Bug do
     bug = Bug.new
     bug.should have(1).error_on(:name)
   end
+
+  describe ".my_bugs" do
+    it "returns only my bugs" do
+      user = User.new
+      bug = Bug.new
+      bug.user_id = user.id
+      Bug.my_bugs.size.should be(1)
+    end
+  end
 end
 
