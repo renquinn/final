823579df2433882040f85a9a035b6cb9cb4a54eb
Ren Quinn <renquinn@gmail.com>
2012-12-03 17:42:37 -0700

    [GREEN] Add salutations to bugs

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,4 +2,8 @@ class Bug < ActiveRecord::Base
   attr_accessible :name, :type, :user_id
   validates_presence_of :name
   scope :my_bugs, lambda { |u| where("user_id= ?", u) }
+
+  def salutations
+    "Hello #{self.name}"
+  end
 end
