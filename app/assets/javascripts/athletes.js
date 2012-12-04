d72296f1055d801f635516c627de4e4b572eda77
Ren Quinn <renquinn@gmail.com>
2012-12-04 16:57:05 -0700

    Remove old athletes javascript

--- app/assets/javascripts/athletes.js
+++
@@ -1,6 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-$('#post_league').live('change', function() {
-  console.log('test');
-  $('#league_form').submit();
-});
